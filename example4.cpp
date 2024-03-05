61a73ecc2f4e8ba92f30bbbb603d3726dcaf4ed9
xiaowuga <729918169@qq.com>
2024-03-06 00:04:41 +0800

    update code

--- example4.cpp
+++ example4.cpp
@@ -16,27 +16,90 @@ int main() {
         face_normal[f] = mesh->compute_face_normal(f);
     }
     int nb_points = mesh->n_vertices();
-    int nb_faces = mesh->n_faces();
-    std::vector<CGAL_Point> sites(nb_points + nb_faces);
+    std::vector<CGAL_Point> cgal_sites(nb_points);
     for(auto v : mesh->vertices()) {
         easy3d::vec3 pos = mesh->position(v);
-        sites[v.idx()] = CGAL_Point(pos.x, pos.y, pos.z);
+        cgal_sites[v.idx()] = CGAL_Point(pos.x, pos.y, pos.z);
     }
-    for(auto f : mesh->faces()) {
-        easy3d::vec3 pos = easy3d::vec3(0, 0, 0);
-        int ct = 0;
-        for(auto v : mesh->vertices(f)) {
-            pos += mesh->position(v); ct++;
-        }
-        pos /= ct;
-        sites[f.idx() + nb_points] = CGAL_Point(pos.x, pos.y, pos.z);
-    }
+    std::vector<easy3d::vec3> sites;
+    UTILS::cgal_points_to_easy3d_points(cgal_sites, sites);
+    easy3d::Box3 box = mesh->bounding_box();
+    double diag = box.diagonal_length();
 
-    Voronoi vor(sites);
+    Voronoi vor(cgal_sites);
     vor.cal();
-    const std::vector<CGAL_Point>& vertices = vor.get_vertices();
-    const std::vector<std::vector<int>>&  vor.get_regions();
-//    RENDER::points_scalar_field_visualization(pp, field);
+    const std::vector<CGAL_Point>& cgal_vertices = vor.get_finite_vertices();
 
+    std::vector<easy3d::vec3> vertices;
+    UTILS::cgal_points_to_easy3d_points(cgal_vertices, vertices);
+
+    const std::vector<std::vector<int>>& regions = vor.get_finite_regions();
+
+
+    std::vector<std::vector<int>> poles(nb_points);
+    for(int i = 0; i < nb_points; i++) {
+        int nb_region = regions[i].size();
+        int flag = 2;
+        easy3d::vec3 vv;
+        for(int j = 0; j < nb_region; j++) {
+
+            if(!box.contains(vertices[regions[i][j]])) {
+                vv = (vertices[regions[i][j]] - sites[i]);
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1) {
+            double maxx = 0;
+            int id = -1;
+            for (int j = 0; j < nb_region; j++) {
+                easy3d::vec3 tmp = vertices[regions[i][j]] - sites[i];
+                if(easy3d::dot(tmp, vv) < 0) {
+                    double len = tmp.norm();
+                    if(maxx > len) {
+                        maxx = len;
+                        id = regions[i][j];
+                    }
+                }
+            }
+            poles[i].emplace_back(id);
+        }
+        else if(flag == 2) {
+            double maxx = 0;
+            int id1 = -1;
+            easy3d::vec3 v1;
+            for (int j = 0; j < nb_region; j++) {
+                easy3d::vec3 tmp = vertices[regions[i][j]] - sites[i];
+                double len = tmp.norm();
+                if(len > maxx) {
+                    maxx = len;
+                    id1 = regions[i][j];
+                    v1 = tmp.normalize();
+                }
+            }
+            maxx = 0;
+            int id2 = -1;
+            for (int j = 0; j < nb_region; j++) {
+                easy3d::vec3 tmp = vertices[regions[i][j]] - sites[i];
+                double len = tmp.norm();
+                if(len > maxx && easy3d::dot(tmp, v1) < 0) {
+                    maxx = len;
+                    id2 = regions[i][j];
+                }
+            }
+            poles[i].emplace_back(id1);
+            poles[i].emplace_back(id2);
+        }
+    }
+
+    easy3d::PointCloud* cloud = new PointCloud;
+    for(int i = 0; i < nb_points; i++) {
+        for(int j = 0; j < poles[i].size(); j++) {
+            cloud->add_vertex(vertices[poles[i][j]]);
+        }
+    }
+    easy3d::Viewer viewer("ASD");
+    viewer.add_model(cloud);
+    viewer.run();
     return 0;
 }
